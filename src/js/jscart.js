d26c5c578ad39366ffadb35bff97388fd09edea7
Chris Park-Kennaby <chrispkennaby@gmail.com>
2018-01-14 21:58:25 +0000

    begin implementing Paypal Express Checkout

--- src/js/jscart.js
+++ src/js/jscart.js
@@ -4,11 +4,32 @@ function Cart(items) {
     this.shippingTotal = 0;
     this.subtotal = 0;
     this.total = 0;
+    this.shippingMatrix = {
+        '1': { largeLetter: 2.50, smallParcel: 3.25 },
+        '2': { largeLetter: 3.80, smallParcel: 7.95 },
+        '3': { largeLetter: 4.5, smallParcel: 8.95 },
+        '4': { largeLetter: 6.5, smallParcel: 11 }
+    };
     this.updateShippingTotal = function () {
-        this.region1 = 2.50;
-        this.region2 = 5;
-        this.region3 = 7.5;
-        this.region3 = 10;
+        if (this.shippingRegion == 1 || this.shippingRegion == 2 || this.shippingRegion == 3 || this.shippingRegion == 4) {
+            var AOCPaperback = this.getItem('AOCPaperback');
+            var AOCAudioCD = this.getItem('AOCAudioCD');
+            var PWPamphlet = this.getItem('PWPamphlet');
+            if (AOCAudioCD.quantity == 0 && AOCPaperback.quantity == 0 && PWPamphlet.quantity == 0) {
+                this.shippingTotal = 0;
+            }
+            else if (AOCAudioCD.quantity == 1 && AOCPaperback.quantity == 0 && PWPamphlet.quantity < 6) {
+                var shippingTotal = this.shippingMatrix[this.shippingRegion].largeLetter;
+                this.shippingTotal = shippingTotal;
+            }
+            else {
+                var shippingTotal = this.shippingMatrix[this.shippingRegion].smallParcel;
+                this.shippingTotal = shippingTotal;
+            }
+        }
+        else {
+            this.shippingTotal = 0;
+        }
     };
     this.updateSubtotal = function () {
         var subtotal = 0;
@@ -27,15 +48,14 @@ function Cart(items) {
         for (var i = 0; i < items.length; i++) {
             var newItem = new this.Item(items[i].id, items[i].price);
             this.items[i] = newItem;
-            //this.addProductBtnEventListener(items[i].id);
+            this.addBuynowEventListener(items[i].id);
             this.addQuantityEventListener(items[i].id);
         }
-        //this.addShippingEventListener();
-        //this.addPayNowBtnEventListener();
-        this.updateCartItems();
-        this.updateCartShipping();
-        this.updateCartTotal();
         this.addShippingEventListener();
+        //this.addPayNowBtnEventListener();
+        this.updateDOMItems();
+        this.updateDOMShipping();
+        this.updateDOMTotal();
         return true;
     };
     this.Item = function (id, price) {
@@ -44,7 +64,7 @@ function Cart(items) {
         this.quantity = 0;
         this.total = 0;
     };
-    this.getCartItem = function (itemId) {
+    this.getItem = function (itemId) {
         for (var i = 0; i < this.items.length; i++) {
             if (this.items[i].id === itemId) {
                 return this.items[i];
@@ -57,52 +77,50 @@ function Cart(items) {
             var el = e.target;
             var itemId = el.id.replace('quantity', ''); // TODO: improve this with data targets in HTML
             var quantity = parseInt(el.value) || 0;
-            var item = this.getCartItem(itemId);
+            var item = this.getItem(itemId);
             item.quantity = quantity;
-            this.updateCartItems();
-            this.updateCartShipping();
-            this.updateCartTotal();
+            this.updateDOMItems();
+            this.updateDOMShipping();
+            this.updateDOMTotal();
         }.bind(this), false);
     };
     // add event listeners to individual product buy now buttons
-    this.addProductBtnEventListener = function (itemId) {
-        // let el:HTMLElement = document.getElementById('quantity' + itemId);
-        // el.addEventListener('input', function(e): void {
-        // 	let el = e.target;
-        // 	let itemId:string = el.id.replace('quantity', ''); // TODO: improve this with data targets in HTML
-        // 	let quantity:number = parseInt(el.value);
-        // 	let item = this.getCartItem(itemId);
-        // 	item.quantity = quantity;
-        // 	this.updateCartShipping();
-        // 	this.updateCartTotal();
-        // }.bind(this), false );
+    this.addBuynowEventListener = function (itemId) {
+        var el = document.getElementById('buynow' + itemId);
+        if (el) {
+            el.addEventListener('click', function (e) {
+                var el = e.target;
+                var itemId = el.id.replace('buynow', ''); // TODO: improve this with data targets in HTML
+                var item = this.getItem(itemId);
+                item.quantity++;
+                var quantityEl = document.getElementById('quantity' + item.id);
+                if (quantityEl) {
+                    quantityEl.value = item.quantity;
+                }
+                else {
+                    console.log('Error: Item total element not found.');
+                }
+                //let cartOffset:number = document.getElementById('buyNow').offsetTop;
+                //console.log(cartOffset);
+                window.scrollTo(0, 2650);
+            }.bind(this), false);
+        }
     };
     // add event listener to shipping radio buttons
     this.addShippingEventListener = function () {
-        // get shipping region
         var el = document.getElementById('shippingOptions');
         var shippingArr = el.getElementsByTagName('input');
         for (var i = 0; i < shippingArr.length; i++) {
             shippingArr[i].addEventListener('change', function (e) {
-                var item = e.target;
-                // if (item.value = 'region1') {
-                // 	console.log('region1');
-                // 	this.shippingRegion = 5;
-                // } else if(item.value = 'region2'){
-                // 	console.log('region2');
-                // 	this.shippingRegion = 10;
-                // } else if(item.value = 'region3'){
-                // 	console.log('region3');
-                // 	this.shippingRegion = 15;
-                // } else if(item.value = 'region4'){
-                // 	console.log('region4');
-                // 	this.shippingRegion = 20;
-                // }			
-                // return shippingRegion = this.shippingRegion;
+                var el = e.target;
+                var shippingRegion = parseInt(el.value);
+                if (this.shippingRegion !== shippingRegion) {
+                    this.shippingRegion = shippingRegion;
+                    this.updateDOMShipping();
+                    this.updateDOMTotal();
+                }
             }.bind(this), false);
         }
-        this.updateCartShipping();
-        this.updateCartTotal();
     };
     // add event listener to pay now / submit button
     this.addPayNowBtnEventListener = function () {
@@ -111,33 +129,41 @@ function Cart(items) {
         // 	let el = e.target;
         // 	let itemId:string = el.id.replace('quantity', ''); // TODO: improve this with data targets in HTML
         // 	let quantity:number = parseInt(el.value);
-        // 	let item = this.getCartItem(itemId);
+        // 	let item = this.getItem(itemId);
         // 	item.quantity = quantity;
-        // 	this.updateCartShipping();
-        // 	this.updateCartTotal();
+        // 	this.updateDOMShipping();
+        // 	this.updateDOMTotal();
         // }.bind(this), false );
     };
-    this.updateCartItems = function () {
+    this.updateDOMItems = function () {
         for (var i = 0; i < this.items.length; i++) {
             // update cart items totals
-            var price = this.items[i].price;
-            var quantity = this.items[i].quantity;
+            var item = this.items[i];
+            var price = item.price;
+            var quantity = item.quantity;
             var total = this.getItemTotal(price, quantity);
-            this.items[i].total = total;
-            this.updateCartItemTotalEl(this.items[i]);
+            item.total = total;
+            // update total
+            var el = document.getElementById('total' + item.id);
+            if (el) {
+                el.innerHTML = '£' + item.total;
+            }
+            else {
+                console.log('Error: Item total element not found.');
+            }
         }
         this.updateSubtotal();
         var subtotal = this.subtotal;
         var subtotalEl = document.getElementById('totalSubTotal');
         subtotalEl.innerHTML = '£' + subtotal;
     };
-    this.updateCartShipping = function (shippingRegion) {
+    this.updateDOMShipping = function (shippingRegion) {
         this.updateShippingTotal();
         var shippingTotal = this.shippingTotal;
         var shippingTotalEl = document.getElementById('totalShippingTotal');
         shippingTotalEl.innerHTML = '£' + shippingTotal;
     };
-    this.updateCartTotal = function () {
+    this.updateDOMTotal = function () {
         this.updateTotal();
         var total = this.total;
         var totalTotalEl = document.getElementById('totalTotal');
@@ -146,18 +172,14 @@ function Cart(items) {
     this.getItemTotal = function (price, quantity) {
         return price * quantity;
     };
-    this.updateCartItemTotalEl = function (item) {
-        var el = document.getElementById('total' + item.id);
-        if (!el) {
-            return false;
-        }
-        el.innerHTML = '£' + item.total;
-        return true;
-    };
 }
-var i1 = { id: 'AOCPaperback', price: 12.5 };
-var i2 = { id: 'AOCAudioCD', price: 10 };
-var i3 = { id: 'PWPamphlet', price: 2.5 };
-var itemArray = [i1, i2, i3];
-var cart = new Cart(itemArray);
+/*
+let i1 = { id:'AOCPaperback', price: 12.5 };
+let i2 = { id: 'AOCAudioCD', price: 10 };
+let i3 = { id: 'PWPamphlet', price: 2.5 };
+
+let itemArray = [i1,i2,i3];
+
+let cart = new Cart(itemArray);
 cart.init();
+*/ 
--- src/js/jscart.ts
+++ src/js/jscart.ts
@@ -17,11 +17,31 @@ function Cart(items: Array<CartItemInput>): void {
 	this.subtotal = 0;
 	this.total = 0;
 
+	this.shippingMatrix = {
+		'1': {largeLetter: 2.50, smallParcel: 3.25},
+		'2': {largeLetter: 3.80, smallParcel: 7.95},
+		'3': {largeLetter: 4.5, smallParcel: 8.95},
+		'4': {largeLetter: 6.5, smallParcel: 11}
+	}
+
 	this.updateShippingTotal = function():void {
-		this.region1 = 2.50;
-		this.region2 = 5;
-		this.region3 = 7.5;
-		this.region3 = 10;
+		if(this.shippingRegion == 1 || this.shippingRegion == 2 || this.shippingRegion == 3 || this.shippingRegion == 4) {
+			let AOCPaperback:CartItem = this.getItem('AOCPaperback');
+			let AOCAudioCD:CartItem = this.getItem('AOCAudioCD');
+			let PWPamphlet:CartItem = this.getItem('PWPamphlet');
+
+			if( AOCAudioCD.quantity == 0 && AOCPaperback.quantity == 0 && PWPamphlet.quantity == 0 ) {
+				this.shippingTotal = 0;
+			} else if ( AOCAudioCD.quantity == 1 && AOCPaperback.quantity == 0 && PWPamphlet.quantity < 6 ) {
+				let shippingTotal:number = this.shippingMatrix[this.shippingRegion].largeLetter;
+				this.shippingTotal = shippingTotal;
+			} else {
+				let shippingTotal:number = this.shippingMatrix[this.shippingRegion].smallParcel;
+				this.shippingTotal = shippingTotal;	
+			}
+		} else {
+			this.shippingTotal = 0;
+		}
 	}
 
 	this.updateSubtotal = function():void {
@@ -41,18 +61,17 @@ function Cart(items: Array<CartItemInput>): void {
 		}
 
 		for(let i:number = 0; i < items.length; i++) {
-			let newItem: CartItem = new this.Item(items[i].id, items[i].price);
+			let newItem:CartItem = new this.Item(items[i].id, items[i].price);
 			this.items[i] = newItem;
-			//this.addProductBtnEventListener(items[i].id);
+			this.addBuynowEventListener(items[i].id);
 			this.addQuantityEventListener(items[i].id);
 		}
-		//this.addShippingEventListener();
+		this.addShippingEventListener();
 		//this.addPayNowBtnEventListener();
 
-		this.updateCartItems();
-		this.updateCartShipping();
-		this.updateCartTotal();
-		this.addShippingEventListener()
+		this.updateDOMItems();
+		this.updateDOMShipping();
+		this.updateDOMTotal();
 		return true;
 	}
 
@@ -63,7 +82,7 @@ function Cart(items: Array<CartItemInput>): void {
 		this.total = 0;
 	}
 
-	this.getCartItem = function(itemId:string):CartItem {
+	this.getItem = function(itemId:string):CartItem {
 		for(let i:number = 0; i < this.items.length; i++) {
 			if (this.items[i].id === itemId) {
 				return this.items[i];
@@ -77,59 +96,54 @@ function Cart(items: Array<CartItemInput>): void {
 			let el = e.target;
 			let itemId:string = el.id.replace('quantity', ''); // TODO: improve this with data targets in HTML
 			let quantity:number = parseInt(el.value) || 0;
-			let item = this.getCartItem(itemId);
+			let item = this.getItem(itemId);
 			item.quantity = quantity;
-			this.updateCartItems();
-			this.updateCartShipping();
-			this.updateCartTotal();
+			this.updateDOMItems();
+			this.updateDOMShipping();
+			this.updateDOMTotal();
 		}.bind(this), false );
 	}
 
 	// add event listeners to individual product buy now buttons
-	this.addProductBtnEventListener = function(itemId: string) {
-		// let el:HTMLElement = document.getElementById('quantity' + itemId);
-		// el.addEventListener('input', function(e): void {
-		// 	let el = e.target;
-		// 	let itemId:string = el.id.replace('quantity', ''); // TODO: improve this with data targets in HTML
-		// 	let quantity:number = parseInt(el.value);
-		// 	let item = this.getCartItem(itemId);
-		// 	item.quantity = quantity;
-		// 	this.updateCartShipping();
-		// 	this.updateCartTotal();
-		// }.bind(this), false );
+	this.addBuynowEventListener = function(itemId: string):void {
+		let el:HTMLElement = document.getElementById('buynow' + itemId);
+		if(el) {
+			el.addEventListener('click', function(e): void {
+				let el = e.target;
+				let itemId:string = el.id.replace('buynow', ''); // TODO: improve this with data targets in HTML
+				let item = this.getItem(itemId);
+				item.quantity++;
+				
+				let quantityEl:HTMLInputElement = (<HTMLInputElement>document.getElementById('quantity' + item.id));
+				if(quantityEl) {
+					quantityEl.value = item.quantity;
+				} else {
+					console.log('Error: Item total element not found.')
+				}
+
+				//let cartOffset:number = document.getElementById('buyNow').offsetTop;
+				//console.log(cartOffset);
+				window.scrollTo(0, 2650);
+			}.bind(this), false );
+		}
 	}
 
 	// add event listener to shipping radio buttons
 	this.addShippingEventListener = function() {
-		// get shipping region
-
 		let el:HTMLElement = document.getElementById('shippingOptions');
 		let shippingArr = el.getElementsByTagName('input');
 		
 		for(let i:number = 0; i < shippingArr.length; i++) {
 			shippingArr[i].addEventListener('change', function(e): void {
-				let item = e.target;
-				// if (item.value = 'region1') {
-				// 	console.log('region1');
-				// 	this.shippingRegion = 5;
-				// } else if(item.value = 'region2'){
-				// 	console.log('region2');
-				// 	this.shippingRegion = 10;
-				// } else if(item.value = 'region3'){
-				// 	console.log('region3');
-				// 	this.shippingRegion = 15;
-				// } else if(item.value = 'region4'){
-				// 	console.log('region4');
-				// 	this.shippingRegion = 20;
-				// }			
-
-				// return shippingRegion = this.shippingRegion;
-
+				let el:HTMLInputElement = e.target;
+				let shippingRegion:number = parseInt(el.value);
+				if(this.shippingRegion !== shippingRegion) {
+					this.shippingRegion = shippingRegion;
+					this.updateDOMShipping();
+					this.updateDOMTotal();
+				}
 			}.bind(this), false );
 		}
-
-		this.updateCartShipping();
-		this.updateCartTotal();
 	}
 
 		
@@ -141,22 +155,29 @@ function Cart(items: Array<CartItemInput>): void {
 		// 	let el = e.target;
 		// 	let itemId:string = el.id.replace('quantity', ''); // TODO: improve this with data targets in HTML
 		// 	let quantity:number = parseInt(el.value);
-		// 	let item = this.getCartItem(itemId);
+		// 	let item = this.getItem(itemId);
 		// 	item.quantity = quantity;
-		// 	this.updateCartShipping();
-		// 	this.updateCartTotal();
+		// 	this.updateDOMShipping();
+		// 	this.updateDOMTotal();
 		// }.bind(this), false );
 	}
 
-	this.updateCartItems = function():void {
+	this.updateDOMItems = function():void {
 		for(let i = 0; i < this.items.length; i++) {
 			// update cart items totals
-			let price:number = this.items[i].price;
-			let quantity:number = this.items[i].quantity;
+			let item:CartItem = this.items[i];
+			let price:number = item.price;
+			let quantity:number = item.quantity;
 			let total:number = this.getItemTotal(price, quantity);
-			this.items[i].total = total;
+			item.total = total;
 
-			this.updateCartItemTotalEl(this.items[i]);
+			// update total
+			let el:HTMLElement = document.getElementById('total' + item.id);
+			if(el) {
+				el.innerHTML = '£' + item.total;
+			} else {
+				console.log('Error: Item total element not found.')
+			}
 		}
 		this.updateSubtotal();
 		let subtotal:number = this.subtotal;
@@ -164,14 +185,14 @@ function Cart(items: Array<CartItemInput>): void {
 		subtotalEl.innerHTML = '£' + subtotal;
 	}
 
-	this.updateCartShipping = function(shippingRegion:number): void {
+	this.updateDOMShipping = function(shippingRegion:number): void {
 		this.updateShippingTotal();
 		let shippingTotal:number = this.shippingTotal;
 		let shippingTotalEl:HTMLElement = document.getElementById('totalShippingTotal');
 		shippingTotalEl.innerHTML = '£' + shippingTotal;
 	}
 
-	this.updateCartTotal = function(): void {
+	this.updateDOMTotal = function(): void {
 		this.updateTotal();
 		let total:number = this.total;
 		let totalTotalEl:HTMLElement = document.getElementById('totalTotal');
@@ -181,17 +202,8 @@ function Cart(items: Array<CartItemInput>): void {
 	this.getItemTotal = function(price: number, quantity: number):number {
 		return price * quantity;
 	}
-
-	this.updateCartItemTotalEl = function(item: CartItem):boolean {
-		let el:HTMLElement = document.getElementById('total' + item.id);
-		if(!el) {
-			return false;
-		}
-		el.innerHTML = '£' + item.total;
-		return true;
-	}
 }
-
+/*
 let i1 = { id:'AOCPaperback', price: 12.5 };
 let i2 = { id: 'AOCAudioCD', price: 10 };
 let i3 = { id: 'PWPamphlet', price: 2.5 };
@@ -200,3 +212,4 @@ let itemArray = [i1,i2,i3];
 
 let cart = new Cart(itemArray);
 cart.init();
+*/
